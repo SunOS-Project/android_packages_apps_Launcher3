4278aae250253ad77b2d08344d3285e4b7e815ba
nift4 <nift4@protonmail.com>
2023-05-07 17:32:18 +0200

    Launcher3: Allow to control parallax effect and center wallpaper
    Change-Id: I2d528f7fb24a91a575c949be60b98696dbedd19f
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- src/com/android/launcher3/settings/SettingsHomescreen.java
+++ src/com/android/launcher3/settings/SettingsHomescreen.java
@@ -130,6 +130,8 @@ public class SettingsHomescreen extends CollapsingToolbarBaseActivity
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { 
         switch (key) {
             case Utilities.KEY_DOCK_SEARCH:
+            case Utilities.KEY_SHORT_PARALLAX:
+            case Utilities.KEY_SINGLE_PAGE_CENTER:
                 LauncherAppState.getInstance(this).setNeedsRestart();
                 break;
             default:
