32827d02c9a3c6a1347be62b6fb7a91e02a8eb94
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-11 09:01:52 +0800

    Launcher3: Expose force monochrome themed icon flag as a user preference
    Change-Id: I81a58671ca5811ea18fb51c7689a0097eb103e87
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>²

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -175,6 +175,7 @@ public final class Utilities {
 
     public static final String KEY_DOCK_SEARCH = "pref_dock_search";
     public static final String KEY_ALLOW_WALLPAPER_ZOOMING = "pref_allow_wallpaper_zooming";
+    public static final String KEY_FORCE_MONOCHROME_ICONS = "pref_forced_monochrome_icons";
 
     /**
      * Returns true if theme is dark.
@@ -918,5 +919,10 @@ public final class Utilities {
     public static boolean canZoomWallpaper(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
         return prefs.getBoolean(KEY_ALLOW_WALLPAPER_ZOOMING, true);
+    }
+
+    public static boolean enableMonoChromeThemedIcons(Context context) {
+        SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
+        return prefs.getBoolean(KEY_FORCE_MONOCHROME_ICONS, false);
     }
 }
