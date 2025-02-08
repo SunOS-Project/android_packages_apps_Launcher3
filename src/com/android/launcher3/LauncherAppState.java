ccd1b96e1f7dea6bd29d67bd7ddc7123d514ba01
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-02-08 23:34:07 +0100

    Launcher3: Improve restart logic
    - Use main executor
    - Use System.exit() instead of killProcess()
    - Reduce wait period
    
    Change-Id: Ibc27b412153110eada13b850b7dfcf32d00a0f2b

--- src/com/android/launcher3/LauncherAppState.java
+++ src/com/android/launcher3/LauncherAppState.java
@@ -219,7 +219,7 @@ public class LauncherAppState implements SafeCloseable {
         // so we force a restart now if needed without waiting for home button press
         if (mNeedsRestart) {
             Toast.makeText(mContext, R.string.restarting_launcher_changes, Toast.LENGTH_SHORT).show();
-            Utilities.restart(mContext);
+            Utilities.restart();
         }
     }
 
